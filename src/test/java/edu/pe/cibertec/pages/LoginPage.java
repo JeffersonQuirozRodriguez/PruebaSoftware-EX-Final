469674370fbdffee644149f94164095b1030889f
Alberth Ruiz <ch.alb.rz@hotmail.com>
2025-12-17 21:00:35 -0500

    Initial commit

---
+++ src/test/java/edu/pe/cibertec/pages/LoginPage.java
@@ -0,0 +1,66 @@
+package edu.pe.cibertec.pages;
+
+import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.AndroidDriver;
+import org.openqa.selenium.WebElement;
+
+public class LoginPage {
+
+    private AndroidDriver driver;
+
+    public LoginPage(AndroidDriver driver){
+        this.driver = driver;
+    }
+    // LOCALIZADORES DE PANTALLA
+    private WebElement getEmailField(){
+        return  driver.findElement(
+                AppiumBy.androidUIAutomator(
+                        "new UiSelector().className(\"android.widget.EditText\").instance(0)"
+                )
+        );
+    }
+
+    private WebElement getPasswordField(){
+        return  driver.findElement(
+                AppiumBy.androidUIAutomator(
+                        "new UiSelector().className(\"android.widget.EditText\").instance(1)"
+                )
+        );
+    }
+
+    private WebElement getLoginButton(){
+        return  driver.findElement(
+                AppiumBy.androidUIAutomator(
+                        "new UiSelector().className(\"android.widget.Button\").instance(1)"
+                )
+        );
+    }
+
+    // ACCIONES DE LOS ELEMENTOS
+
+    public void enterEmail(String email){
+        getPasswordField().click();
+        getEmailField().clear();
+        getEmailField().sendKeys(email);
+    }
+
+    public void enterPassword(String password)
+    {
+        getPasswordField().click();
+        getPasswordField().clear();
+        getPasswordField().sendKeys(password);
+    }
+
+    public void clickLoginButton (){
+        getLoginButton().click();
+    }
+
+    public void login(String email, String password){
+        enterEmail(email);
+        enterPassword(password);
+        clickLoginButton();
+    }
+
+
+
+}
---
+++ src/test/java/edu/pe/cibertec/steps/LoginSteps.java
@@ -0,0 +1,64 @@
+package edu.pe.cibertec.steps;
+
+import edu.pe.cibertec.config.AppiumConfig;
+import edu.pe.cibertec.pages.LoginPage;
+import io.appium.java_client.android.AndroidDriver;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    private static AndroidDriver driver;
+    private static LoginPage loginPage;
+
+    @Before
+    public void setUp(){
+        System.out.println("INICIANDO DRIVER y PAGE OBJECTS");
+        driver = AppiumConfig.getDriver();
+        loginPage = new LoginPage(driver);
+        System.out.println("DRIVER INICIADO: " + (driver!=null));
+        System.out.println("LOGIN PAGE INICIADO:" + (loginPage!=null));
+    }
+
+    @After
+    public void tearDown(){
+        System.out.println("CERRANDO DRIVER");
+        AppiumConfig.quitDriver();
+        driver = null;
+        loginPage = null;
+    }
+
+    @Given("que el usuario esta en la pantalla de login")
+    public void queElUsuarioEstaEnLaPantallaDeLogin(){
+        System.out.println("USUARIO EN PANTALLA LOGIN");
+    }
+
+    @When("ingresa el email {string}")
+    public void ingresaElEmail(String email){
+        loginPage.enterEmail(email);
+    }
+
+    @And("ingresa el password {string}")
+    public void ingresElPassword(String password){
+        loginPage.enterPassword(password);
+    }
+
+    @And("hacer clic en el boton login")
+    public void haceClicEnElBotonLogin(){
+        loginPage.clickLoginButton();
+    }
+
+    @Then("deberia acceder a la pantalla principal")
+    public void deberiaAccederALaPantallaPrincipal(){
+        System.out.println("VALIDANDO ACCESO PANTALLA PRINCIPAL");
+    }
+
+    @Then("deberia ver un mensaje de error")
+    public void deberiaVerUnMensajeDeError(){
+        System.out.println("VALIDANDO MENSAJE DE ERROR");
+    }
+}
